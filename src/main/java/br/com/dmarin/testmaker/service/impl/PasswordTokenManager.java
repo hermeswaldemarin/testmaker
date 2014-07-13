55c2ba9615fb1e4a07832f89e378ae34972a0e8d
Hermes Waldemarin Neto <hermesneto@plusoft.com.br>
2014-07-13 18:14:46 -0300

    recreating project

---
+++ src/main/java/br/com/dmarin/testmaker/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package br.com.dmarin.testmaker.service.impl;
+
+import br.com.dmarin.testmaker.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
